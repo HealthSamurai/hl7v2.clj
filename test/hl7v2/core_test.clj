f2ec6daa2cee380eacefc36406c3ca9519150f2b
Vlad Bokov <razum2um@mail.ru>
2019-09-16 04:04:12 +0300

    Fix separate insupance segment test

--- test/hl7v2/core_test.clj
+++ test/hl7v2/core_test.clj
@@ -128,25 +128,14 @@ IN2||354221840|0000007496^RETIRED|||||||||||||||||||||||||||||||||Y|||CHR||||W||
      (sut/parse-segment
       ctx "IN1|1|303401^PRIV HLTH CARE SYS-BCBS OF N CAROLINA|3034|BCBS OF NORTH CAROLINA|CLMS PROCESSING CONTRACTOR PO BOX 9518^^DURHAM^NC^32145-9518^||(845)543-3876^^^^^845^5433876|1233||||20160726||||UPGRADETEST^CPAP^^|Self|19490512|876 MAIN^^BANANA VALLEY^WA^98038^US^^^KING|||1**1|||NO||||20170726102055|BARLLH1^BARLOW^LOUIS^H.^|||||4694998|F8086412450||||||Full|M|1258 ROSE AVE SW^^RENTON^WA^98057^US|Verified Pat||BOTH||")
 
-     ["IN1" {:relationship_to_patient {:code "Self"}, :period_start "20160726",
-             :address [{:street {:text "876 MAIN"}, :city "BANANA VALLEY", :state "WA",
-                        :postal_code "98038", :country "US", :county "KING"}],
-             :phone_number [{:phone "(845)543-3876", :area_city "845", :local_number "5433876"}],
-             :policy_number "F8086412450", :eligibility_flag "NO", :set_id "1", :group_number "1233",
-             :beneficiary [{:family {:surname "UPGRADETEST"}, :given "CPAP"}],
-             :employment_status {:code "Full"}, :coverage "BOTH",
-             :company_name [{:name "BCBS OF NORTH CAROLINA"}],
-             :company_address [{:street {:text "CLMS PROCESSING CONTRACTOR PO BOX 9518"},
-                                :city "DURHAM", :state "NC", :postal_code "32145-9518"}],
-             :payor [{:id "3034"}], :birth_date {:time "19490512"},
-             :verification_datetime {:time "20170726102055"},
+     ["IN1" {:beneficiary_address [{:city "BANANA VALLEY", :country "US", :county "KING", :postal_code "98038", :state "WA", :street {:text "876 MAIN"}}], :beneficiary_birthDate {:time "19490512"}, :beneficiary_gender "M",
+             :beneficiary_name [{:family {:surname "UPGRADETEST"}, :given "CPAP"}], :benifits_coordination_priority "1**1", :class_type "BOTH", :contract_identifier "F8086412450", :eligibility_flag "NO",
+             :employer_address [{:city "RENTON", :country "US", :postal_code "98057", :state "WA", :street {:text "1258 ROSE AVE SW"}}], :employment_status {:code "Full"}, :group_number "1233",
              :identifier_type {:code "303401", :display "PRIV HLTH CARE SYS-BCBS OF N CAROLINA"},
-             :gender "M", :benifits_coordination_priority "1**1",
-             :employer_address [{:street {:text "1258 ROSE AVE SW"}, :city "RENTON", :state "WA",
-                                 :postal_code "98057", :country "US"}],
-             :company_plan "4694998", :verification_status "Verified Pat",
-             :verification_by [{:id "BARLLH1", :family {:surname "BARLOW"},
-                                :given "LOUIS", :initials "H."}]}]
+             :payor_organization_address [{:city "DURHAM", :postal_code "32145-9518", :state "NC", :street {:text "CLMS PROCESSING CONTRACTOR PO BOX 9518"}}],
+             :payor_organization_contact_telecom [{:area_city "845", :local_number "5433876", :phone "(845)543-3876"}], :payor_organization_identifier_value [{:id "3034"}],
+             :payor_organization_name [{:name "BCBS OF NORTH CAROLINA"}], :period_start "20160726", :relationship_to_patient {:code "Self"}, :set_id "1",
+             :verification_by [{:family {:surname "BARLOW"}, :given "LOUIS", :id "BARLLH1", :initials "H."}], :verification_datetime {:time "20170726102055"}, :verification_status "Verified Pat"}]
      )
     ))
 
