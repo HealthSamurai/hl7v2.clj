504f99bc878f21d06f9ccf735e05cc644124bacf
niquola <niquola@gmail.com>
2020-02-04 22:55:54 +0300

    segments progress

--- test/hl7v2/segment_test.clj
+++ test/hl7v2/segment_test.clj
@@ -73,7 +73,87 @@
      :race [{:display "White"}],
      :telecom
      [{:email "users@gmail.com", :phone "33333333", :system "phone", :use "home"}
-      {:phone "333333333", :system "phone", :use "work"}]}]
+      {:phone "333333333", :system "phone", :use "work"}]}])
+
+  (matcho/match
+   (sut/parse-segment
+    separators
+    "IN1|1|11111111^AMB-ANTHEM BLUE ACCESS-BRANSO-BBA^^^AMB-ANTHEM BLUE ACCESS-BRANSO-BBA|222222|Missouri Blue Cross Blue Shield|PO BOX 33333^ANYWHERE^ATLANTA^GA^44444^CD:55555^B||1111111^WPN^CD:6666|77777||88888||20190101000000|||CD:999|john^doe^K^^^^CURRENT|CD:211111|2222222|2333333 STATE HIGHWAY T^^BTANTOT^MO^244444^CD:25555^M~2666 STATE HIGHWAY T^^BTANTOT^MO^27777^CD:28888^H~USER@COM.COM^^^^^^CD:2888|||1|||||||||||||CD:2999||||||||F|Anywhere^^Anywhere^MO^311111^CD:22133^B|||||A3222222")
+   ["IN1"
+    {:_seqno "1",
+     :beneficiary
+     {:address [{:city "BTANTOT",
+                 :country "CD:25555",
+                 :postal_code "244444",
+                 :state "MO",
+                 :street {:text "2333333 STATE HIGHWAY T"},
+                 :type "M"}
+                {:city "BTANTOT",
+                 :country "CD:28888",
+                 :postal_code "27777",
+                 :state "MO",
+                 :street {:text "2666 STATE HIGHWAY T"},
+                 :type "H"}
+                {:street {:text "USER@COM.COM"}, :type "CD:2888"}],
+      :birth_date {:time "2222222"},
+      :employer {:address [{:city "Anywhere",
+                            :country "CD:22133",
+                            :postal_code "311111",
+                            :state "MO",
+                            :street {:text "Anywhere"},
+                            :type "B"}]},
+      :gender "F",
+      :name [{:family {:surname "john"},
+              :given "doe",
+              :initials "K",
+              :type "CURRENT"}],
+      :relationship {:code "CD:211111"},
+      :subscriber_id [{:value "A3222222"}]},
+     :benifits {:priority "1"},
+     :company_plan "CD:2999",
+     :group {:employee {:identifier [{:value "88888"}]}, :id "77777"},
+     :payor {:address [{:city "ATLANTA",
+                        :country "CD:55555",
+                        :postal_code "44444",
+                        :state "GA",
+                        :street {:text "PO BOX 33333"},
+                        :text "ANYWHERE",
+                        :type "B"}],
+             :contact {:telecom [{:phone "1111111", :type "CD:6666", :use "WPN"}]},
+             :identifier [{:value "222222"}],
+             :name [{:name "Missouri Blue Cross Blue Shield"}]},
+     :period {:start "20190101000000"},
+     :plan {:identifier {:alternate_display "AMB-ANTHEM BLUE ACCESS-BRANSO-BBA",
+                         :code "11111111",
+                         :display "AMB-ANTHEM BLUE ACCESS-BRANSO-BBA"},
+            :type "CD:999"}}]
    )
+  
+
+  (matcho/match
+   (sut/parse-segment
+    separators
+    "IN2|empid:11111|ssn:222|||||||||||||||||||||||payor:3333||||||||||||||||||||||||||||||||||||mem:3333||(555)5555-4384^PRN^CD:193972~(555)555-4384^CP^CD:78193972~(555)555-4384^CP^CD:78193972|(111)111-1111^WPN||||||||CD:24246")
+   ["IN2"
+    {:beneficiary {:employee {:identifier [{:value "empid:11111"}]},
+                   :identifier [{:system "ssn", :value "ssn:222"}
+                                {:system "member-number" :value "mem:3333"}]
+                   :telecom
+                   [{:phone "(555)5555-4384",
+                     :system "phone",
+                     :type "CD:193972",
+                     :use "PRN"}
+                    {:phone "(555)555-4384",
+                     :system "phone",
+                     :type "CD:78193972",
+                     :use "CP"}
+                    {:phone "(555)555-4384",
+                     :system "phone",
+                     :type "CD:78193972",
+                     :use "CP"}
+                    {:phone "(111)111-1111", :system "phone", :use "WPN"}]},
+     :cms {:relationship {:code "CD:24246"}},
+     :payor {:id [{:value "payor:3333"}]}}])
+
   )
 
